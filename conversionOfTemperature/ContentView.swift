a69bd3d882cfb7fd0f48f5479e043d52c30085a8
selinurkmez <selinurkmeez@gmail.com>
2024-08-03 16:55:57 +0300

    Initial Commit

---
+++ conversionOfTemperature/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  conversionOfTemperature
+//
+//  Created by macbook on 3.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ conversionOfTemperature/conversionOfTemperatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  conversionOfTemperatureApp.swift
+//  conversionOfTemperature
+//
+//  Created by macbook on 3.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct conversionOfTemperatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
