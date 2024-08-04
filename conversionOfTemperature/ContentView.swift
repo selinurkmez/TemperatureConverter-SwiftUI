80534df6599e2a26065263d4d01ef41b6e3a632a
selinurkmez <selinurkmeez@gmail.com>
2024-08-04 14:01:56 +0300

    Initial commit

--- conversionOfTemperature/ContentView.swift
+++ conversionOfTemperature/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var celcius = "0"
+    
+    var fahrenheit: String {
+        if let celsiusValue = Double(celcius) {
+            let fahrenheitValue = (celsiusValue * 9/5) + 32
+            return String(format: "%.2f", fahrenheitValue)
+        } else {
+            return "Geçersiz giriş"
+        }
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            
+            Spacer()
+            
+            HStack {
+                TextField("Celcius:", text: $celcius)
+                    .padding()
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                Text("Fahrenheit: \(fahrenheit)")
+            }
+            
+            Spacer()
+        }
+        .padding()
     }
 }
 
@@ -19,3 +45,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
